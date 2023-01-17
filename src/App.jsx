57ce5937285fed2b8e792ee043aa1e1d489cac89
aishacodes <aishatabdulfatah81@gmail.com>
2023-01-17 12:21:45 +0100

    feat: project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>Navbar</nav>i
+      <div className="hero">
+        <img src={Logo} alt="" />
+        <img src="/images/artwork.png" alt="" />
+      </div>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
